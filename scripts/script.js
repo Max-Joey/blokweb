197df666be35d4ad1293e8b398635e0ed49701b6
maxvl3 <94384526+maxvl3@users.noreply.github.com>
2021-12-11 18:39:39 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+var deButton = document.querySelector("header nav button");
+var closeButton = document.querySelector("header div button");
+ 
+function doeFormNeerEnOp(){
+   let hetFormulier = document.querySelector("header div");
+   hetFormulier.classList.toggle("toonMenu");
+}
+ 
+deButton.addEventListener("click", doeFormNeerEnOp);
+closeButton.addEventListener("click", doeFormNeerEnOp);
+
+
+
+var dropdownButton = document.querySelector("main section:nth-of-type(1) div button");
+
+function dropdownUitklappen(){
+	let dropdownList = document.querySelector("main section:nth-of-type(1) div ul");
+	dropdownList.classList.toggle("dropdown");
+}
+
+dropdownButton.addEventListener("click", dropdownUitklappen);
